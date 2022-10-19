ca9608366188298238a0dbc4d10042060e43d672
SantiagoVira <santiagovira06@gmail.com>
2022-10-19 11:11:33 -0400

    Last fixes

--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class LinearEquationRunner {
     public static void main(String[] args) {
+        start();
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter coordinate 1: ");
@@ -12,14 +13,23 @@ public class LinearEquationRunner {
         String coord2 = input.nextLine();
         int[] xy2 = coordinateToXY(coord2);
 
-        LinearEquation linEq = new LinearEquation(xy1[0], xy1[1], xy2[0], xy2[1]);
+        if (xy1[0] == xy2[0]) {
+            System.out.println("These points share a vertical line, x = " + xy1[0]);
+        } else {
+            LinearEquation linEq = new LinearEquation(xy1[0], xy1[1], xy2[0], xy2[1]);
 
-        System.out.print(linEq.lineInfo());
+            System.out.print(linEq.lineInfo());
 
-        System.out.print("\nEnter a value for x: ");
-        double xValue = input.nextDouble();
-        System.out.println("\nThe point on the line is " + linEq.coordinateForX(xValue));
+            System.out.print("\nEnter a value for x: ");
+            double xValue = input.nextDouble();
+            System.out.println("\nThe point on the line is " + linEq.coordinateForX(xValue));
+        }
 
+
+    }
+
+    public static void start() {
+        System.out.println("Welcome to the Linear Equation Runner Program!");
     }
 
     // Turn a string in form "(x, y)" to an integer array of the numbers x and y
